deb141bef301dc4fa211315b72a5c35bc287625a
jeremyferg <jeremy.ferguson573@gmail.com>
2024-03-02 01:26:18 -0600

    fits with out-of-control recipe

--- project_scripts/4_fit_tune_scripts/4_tune_boostedtree.R
+++ project_scripts/4_fit_tune_scripts/4_tune_boostedtree.R
@@ -6,6 +6,7 @@
 ###############################################
 ###############################################
 
+## NOTE: Seed used for randomization
 
 #####################
 ##### Libraries #####
@@ -25,7 +26,7 @@ doParallel::registerDoParallel(detectCores(logical = TRUE))
 ##### Data Sets #####
 #####################
 
-load(here('recipes/nba_recipe_general.rda'))
+load(here('recipes/nba_recipe_outta.rda'))
 load(here("data/splits_folds/nba_seasons_folds_base.rda"))
 load(here('results/keep_wflow_rsample.rda'))
 
@@ -33,5 +34,48 @@ load(here('results/keep_wflow_rsample.rda'))
 ################################################################################
 ################################################################################
 
+# model specifications ----
+bt_spec <- 
+  boost_tree(
+    trees = tune(),
+    learn_rate = tune(), 
+    min_n = tune(),
+    mtry = tune(),
+    
+  ) |> 
+  set_mode('regression') |> 
+  set_engine("xgboost")
 
+# define workflows ----
+bt_outta_wflow <- 
+  workflow() |> 
+  add_model(bt_spec) |> 
+  add_recipe(nba_recipe_outta)
+
+# hyperparameter tuning values ----
+
+extract_parameter_set_dials(bt_spec)
+
+bt_outta_params <- extract_parameter_set_dials(bt_spec) |> 
+  update(mtry = mtry(c(1, 15)),
+         min_n = min_n(range = c(1, 10)),
+         learn_rate = learn_rate(range = c(1/10, 19/20)),
+         trees = trees(range = c(100, 1000)))
+
+# build tuning grid
+bt_grid <- grid_regular(bt_outta_params, levels = 5)
+
+# fit workflows/models ----
+set.seed(57879)
+bt_outta_tuned <- 
+  bt_outta_wflow |> 
+  tune_grid(
+    nba_seasons_folds_base, 
+    grid = bt_grid,
+    control = keep_wflow_rsample
+  )
+
+# write out results (fitted/trained workflows) ----
+
+save(bt_outta_tuned, file = here('results/bt_outta_tuned.rda'))
 
