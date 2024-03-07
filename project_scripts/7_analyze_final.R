fd868f4e6957b5acf9afa789c352c0e8d28323dd
jeremyferg <jeremy.ferguson573@gmail.com>
2024-03-07 15:15:53 -0600

    starting final fit analysis

--- project_scripts/7_analyze_final.R
+++ project_scripts/7_analyze_final.R
@@ -21,9 +21,23 @@ tidymodels_prefer()
 ##### Data Sets #####
 #####################
 
+load(here('results/final_fit.rda'))
+nba_seasons_test <- read_rds(here('data/splits_folds/nba_seasons_train.rds'))
 
+################################################################################
+################################################################################
+################################################################################
 
-################################################################################
-################################################################################
-################################################################################
+# predicting  
+seasons_test_res <- predict(final_fit, nba_seasons_test)
+
+# binding prediction with actual value
+seasons_test_res <- 
+  bind_cols(seasons_test_res, nba_seasons_test) |> 
+  select(adj_salary, .pred)
+
+seasons_test_res
+
+# metric set tibble
+rmse(seasons_test_res, truth = adj_salary, estimate = .pred)
 
