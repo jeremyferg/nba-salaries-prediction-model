db1d023e2ff26a07b332d4fe04f0bf5a7b0153b1
jeremyferg <jeremy.ferguson573@gmail.com>
2024-03-06 19:37:34 -0600

    temporarily put model analysis on separate scripts

---
+++ project_scripts/5_model_analysis_nonlinear.R
@@ -0,0 +1,84 @@
+######################################
+######################################
+#######                       ########
+######      MODEL ANALYSIS     #######
+#######                       ########
+######################################
+######################################
+
+
+#####################
+##### Libraries #####
+#####################
+
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+tidymodels_prefer()
+
+#####################
+##### Data Sets #####
+#####################
+
+load(here("results/nonlinear_recipe/ann_nonlinear_tuned.rda"))
+load(here("results/nonlinear_recipe/bt_nonlinear_tuned.rda"))  
+load(here("results/nonlinear_recipe/enet_nonlinear_tuned.rda"))    
+load(here("results/nonlinear_recipe/lm_fit_folds_nonlinear.rda"))  
+load(here("results/nonlinear_recipe/mars_nonlinear_tuned.rda"))
+load(here("results/nonlinear_recipe/null_fit_folds_nonlinear.rda"))
+load(here("results/nonlinear_recipe/rf_nonlinear_tuned.rda"))   
+
+################################################################################
+################################################################################
+################################################################################
+
+
+rbind(
+  
+  ann_outta_tuned |> 
+    show_best(metric = 'rmse') |> 
+    slice_head() |> 
+    select(c(.metric, mean, n, std_err)) |>
+    mutate(model = 'ANN', .before = .metric),
+  
+  bt_outta_tuned |> 
+    show_best(metric = 'rmse') |> 
+    slice_head() |> 
+    select(c(.metric, mean, n, std_err)) |>
+    mutate(model = 'Boosted Trees', .before = .metric),
+  
+  enet_outta_tuned |> 
+    show_best(metric = 'rmse') |> 
+    slice_head() |> 
+    select(c(.metric, mean, n, std_err)) |>
+    mutate(model = 'Elastic Net', .before = .metric),
+  
+  lm_fit_folds |> 
+    show_best(metric = 'rmse') |> 
+    slice_head() |> 
+    select(c(.metric, mean, n, std_err)) |>
+    mutate(model = 'OLS', .before = .metric),
+  
+  mars_outta_tuned |> 
+    show_best(metric = 'rmse') |> 
+    slice_head() |> 
+    select(c(.metric, mean, n, std_err)) |> 
+    mutate(model = 'MARS', .before = .metric),
+  
+  null_fit_folds |> 
+    show_best(metric = 'rmse') |> 
+    slice_head() |> 
+    select(c(.metric, mean, n, std_err)) |> 
+    mutate(model = 'Null', .before = .metric),
+  
+  rf_outta_tuned |> 
+    show_best(metric = 'rmse') |> 
+    slice_head() |> 
+    select(c(.metric, mean, n, std_err)) |> 
+    mutate(model = 'Random Forest', .before = .metric)
+  
+)
+  
+enet_outta_tuned |> collect_metrics(metric = 'rmse') |> 
+  print(n = 50)
