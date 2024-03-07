fddcf2ffa40f9ba5e2ff2670602c24a561e21a6e
jeremyferg <jeremy.ferguson573@gmail.com>
2024-03-07 15:04:51 -0600

    fitting best model

--- project_scripts/6_fit_final.R
+++ project_scripts/6_fit_final.R
@@ -25,11 +25,23 @@ doParallel::registerDoParallel(detectCores(logical = TRUE))
 ##### Data Sets #####
 #####################
 
-load(here('recipes/nba_recipe_general.rda'))
-load(here("data/splits_folds/nba_seasons_folds_base.rda"))
-load(here('results/keep_wflow_rsample.rda'))
+load(here("results/interact_recipe/rf_interact_tuned.rda"))  
+nba_seasons_train <- read_rds(here('data/splits_folds/nba_seasons_train.rds'))
 
 ################################################################################
 ################################################################################
 ################################################################################
+
+# finalize workflow ----
+final_wflow <-
+  rf_outta_tuned |> 
+  extract_workflow(rf_outta_tuned) |> 
+  finalize_workflow(select_best(rf_outta_tuned, metric = 'rmse'))
+
+# train final model ----
+final_fit <- fit(final_wflow, nba_seasons_train)
+
+# save fit model ----
+save(final_fit, file = here('results/final_fit.rda'))
+
 
